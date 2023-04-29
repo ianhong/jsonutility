61da2864e247a31e8b94e27c3f5213b949966cd3
Ian Hong <ihong@costco.com>
2023-04-29 11:28:41 -0700

    init

---
+++ json/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+var schema = await JsonSchema.FromFileAsync(@"F:\\src\ecomm-nc-omsA\Source\Costco.BizTalk.OrderManagement.Import\Schemas\WebOrder.json");
+ var generator = new CSharpGenerator(schema);
+var file = generator.GenerateFile(); 
+
+var tw = new StreamWriter("csharp.cs");
+tw.Write(file);
+tw.Close();
